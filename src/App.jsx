3aacfd29feedb41efb17c6264f7e018818251571
Allan Pinheiro <allan.hal@gmail.com>
2024-07-17 20:04:28 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import ProductCard from "./components/ProductCard";
+
+function App() {
+  return (
+    <div>
+      <h1>Componentização</h1>
+
+      <ProductCard />
+
+      <ProductCard />
+
+      <ProductCard />
+
+      <ProductCard />
+
+      <ProductCard />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,13 @@
+export default function ProductCard() {
+    return (
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuwvRXQOvbENimyKnrB_vSBquQYYatsY4JPQ&s"
+          alt="tenis"
+        />
+        <p>Titulo: Tênis Caro</p>
+        <p>Preço: R$ 20,00</p>
+        <p>Preço com desconto: R$ 10,00</p>
+      </div>
+    );
+  }
